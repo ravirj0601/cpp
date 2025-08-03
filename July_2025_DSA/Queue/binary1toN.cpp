723e80c58894e6e018647dc17fc312cc0cdc048e
ravirj0601 <ravirj0601@gmail.com>
2025-08-03 23:56:41 +0530

    New Changes from today

--- July_2025_DSA/Queue/binary1toN.cpp
+++ July_2025_DSA/Queue/binary1toN.cpp
@@ -2,6 +2,21 @@
 #include<queue>
 using namespace std;
 
+class binary1toN
+{
+
+public:
+    binary1toN(/* args */);
+    ~binary1toN();
+};
+
+binary1toN::binary1toN(/* args */)
+{
+}
+
+binary1toN::~binary1toN()
+{
+}
 
 
 int main(int argc, char const *argv[])
