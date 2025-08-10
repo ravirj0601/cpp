eacef64d4c8e4596391207895ffea8b27be7b093
ravirj0601 <ravirj0601@gmail.com>
2025-08-10 21:42:36 +0530

    Made some progress in linkedlist reversing bruteforce done need to improve

--- July_2025_DSA/linkedList/reverseLL.cpp
+++ July_2025_DSA/linkedList/reverseLL.cpp
@@ -14,23 +14,63 @@ public:
 
 };
 
-void reverseList(){
-    if (head == nullptr) {
-        cerr << "Empty list...\n";
-        return;
-    }
-    Node *prev = nullptr;
+Node* reverseList(Node* head){
+    vector<int>nodes;
     Node *curr = head;
-    while(curr != nullptr){
+    while(curr != NULL){
+        nodes.push_back(curr->value);
         curr = curr->next;
-        prev = curr;
-        :w
+    }
+    int i = nodes.size()-1;
+    curr = head;
+    while(i >= 0){
+        curr->value = nodes[i];
+        i--;
+        curr = curr->next;
     }
 
+    return head;
 }
 
+Node* createNodeList(vector<int> list){
+    Node *head = nullptr;
+    Node *curr = head;
+    int i = 0;
+    int n = list.size() - 1;
+    while(i <= n){
+        Node *temp = new Node(list[i]);
+        if (head ==  nullptr) {
+            head = temp;
+            curr = temp;
+        }else{
+            curr->next = temp;
+            curr = temp;
+        }
+        i++;
+    }
+    return head;
+}
+ void printLL(Node* head){
+    if(head == nullptr){
+        cerr << "!No Data... Empty List..\n";   
+        return;
+    }
+    cout << "List Values: ";
+    Node* curr = head;
+    while(curr != NULL){
+        cout << curr->value << " " ;
+        curr = curr->next;
+    }
+    cout << "\n";
+}
+
+
 int main() {
-    
+    vector<int> lists = {1,2,3,4,5};
+    Node *head = createNodeList(lists);
+    printLL(head);
+    Node *newhead = reverseList(head);
+    printLL(newhead);
     return 0;
 }
 
