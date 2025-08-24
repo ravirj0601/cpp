142975b290666b1896ed26bc07f0a676cc908962
ravirj0601 <ravirj0601@gmail.com>
2025-08-24 15:59:10 +0530

    feat: Implement insertLast function and update main for testing

--- July_2025_DSA/linkedList/insertBegining.cpp
+++ July_2025_DSA/linkedList/insertBegining.cpp
@@ -19,12 +19,28 @@ class ListOps{
             head = nullptr;
         }
 
+        // Insert from begining of linkedList
         void insertFront(int val){
             Node* temp = new Node(val);
             temp->next = head;
             head = temp;
         }
 
+        // Insert element from last 
+        void insertLast(int val){
+            Node* temp = new Node(val);
+            Node* curr = head;
+            if (head == nullptr) {
+                head = temp;
+                return;
+            }
+            while (curr->next != nullptr) {
+                curr = curr->next;
+            }
+            curr->next = temp;
+        }
+
+        // For displaying element of linkedList
         void display(){
             Node* temp = head;
             while(temp != nullptr){
@@ -48,6 +64,8 @@ int main() {
     mylist.insertFront(10);
     mylist.insertFront(20);
     mylist.insertFront(30);
+    mylist.insertLast(40);
+    mylist.insertLast(50);
     cout << "Linked List: ";
     mylist.display();
 
