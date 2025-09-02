8aec3fe3681db20312a6bfe55502f4e3bc7f486e
ravirj0601 <ravirj0601@gmail.com>
2025-09-02 23:09:12 +0530

    Today minimal changes

--- July_2025_DSA/linkedList/lruCahche.cpp
+++ July_2025_DSA/linkedList/lruCahche.cpp
@@ -4,22 +4,16 @@ using namespace std;
 /**
  * 
  *  LRU Cache
- *  Implement the Least Recently Used (LRU) cache class LRUCache. The class should support the following operations
+ *  Implement the Least Recently Used (LRU) cache class LRUCache.//
+ *  The class should support the following operations
  *  LRUCache(int capacity) Initialize the LRU cache of size capacity.
  *  int get(int key) Return the value corresponding to the key if the key exists, otherwise return -1.
  *  void put(int key, int value) Update the value of the key if the key exists. Otherwise, 
- *  add the key-value pair to the cache. If the introduction of the new pair causes the cache to exceed its capacity, remove the least recently used key.
+ *  add the key-value pair to the cache. If the introduction of the new pair causes the //
+ *  cache to exceed its capacity, remove the least recently used key.
  *  A key is considered used if a get or a put operation is called on it.
  *
  *  Ensure that get and put each run in O(1) average time complexity.
- *
- *  Example 1:
- *
- *  Input:
- *  ["LRUCache", [2], "put", [1, 10],  "get", [1], "put", [2, 20], "put", [3, 30], "get", [2], "get", [1]]
- *
- *  Output:
- *  [null, null, 10, null, null, 20, -1]
  *
  *  Explanation:
  *  LRUCache lRUCache = new LRUCache(2);
@@ -38,6 +32,31 @@ using namespace std;
  *      -   
  *
  * */
+
+class Node{
+public:
+    int key;
+    int value;
+    Node* prev;
+    Node* next;
+    Node(int k, int v):key(k), value(v), prev(nullptr), next(nullptr){};
+};
+
+class LRUCache{
+    private:
+        int capacity;
+        int key;
+        int value;
+        Node* head;
+        Node* tail;
+
+        void push_to_front(Node* newNode){
+            if (head == newNode && tail == newNode) {
+                
+            }
+
+        }
+};
 
 int main() {
     
