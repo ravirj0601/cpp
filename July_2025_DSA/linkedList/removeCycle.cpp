8af9e60cdaf7a966a656b04e0ce588d7569f1973
ravirj0601 <ravirj0601@gmail.com>
2025-09-28 18:53:26 +0530

    Remove circular list..

--- July_2025_DSA/linkedList/removeCycle.cpp
+++ July_2025_DSA/linkedList/removeCycle.cpp
@@ -12,11 +12,25 @@ class Node{
 class NodeOps{
     private:
         Node* head;
+        Node* meetNode_{nullptr};
         void clear() noexcept{
             Node* newNode = head->next;
             delete head;
             head = newNode;
         }
+
+        Node* findCycleStart(){
+            if (!meetNode_ || !head) return nullptr;
+            Node* ptr1 = head;
+            Node* ptr2 = meetNode_;
+            while (ptr1 != ptr2) {
+                ptr1 = ptr1->next;
+                ptr2 = ptr2->next;
+                cout << ptr1->val_ << " ";
+            }
+            return ptr1;
+        }
+
     public:
         NodeOps():head(nullptr){}
         ~NodeOps(){clear();}
@@ -40,16 +54,18 @@ class NodeOps{
         }
         
         bool detectCycle(){
+            meetNode_ = nullptr;
             if (head == nullptr) {
                 cout << "List is empty to check Cycle!\n";
                 return false;
             }
             Node* slow = head;
-            Node* fast = head->next->next;
-            while (slow != nullptr && slow->next!=nullptr) {
+            Node* fast = head;
+            while (fast != nullptr && fast->next != nullptr) {
                 slow = slow->next;
                 fast = fast->next->next;
-                if (slow->val_ != fast->val_) {
+                if (slow == fast) {
+                    meetNode_ = slow;
                     return true; 
                 }
             }
@@ -63,15 +79,78 @@ class NodeOps{
              * find last node of cycle 
              * and put null to last node next
              */
+            if (!this->detectCycle()){
+                cout << "This list is not circular..!";
+                return;
+            }
+            Node* cycleStart = findCycleStart();
+            Node* startNode = cycleStart;
+            while (startNode->next != cycleStart) {
+                startNode = startNode->next;
+            }
+            startNode->next = nullptr;
+        }
+
+        void printCircularList(){
+            if (head == nullptr) {
+                cout << "List is empty\n";
+                return;
+            }
+            cout << "Circular list Elements: ";
+            Node* curr = head;
+            do {
+                cout << curr->val_ << " ";
+                curr = curr->next;
+            }while (curr != head);
+
+            cout << "\n";
         }
 
         void printElement(){
-            
+            if (head == nullptr) {
+                cout << "List is empty\n";
+                return;
+            }
+            cout << "List Elements: ";
+            Node* curr = head;
+            while (curr != nullptr){
+                cout << curr->val_ << " ";
+                curr = curr->next;
+            }  
+            cout << "\n";
+        }
+
+        int printMeetNode(){
+            return meetNode_->val_;
+        }
+        
+        int printStartNode(){
+            return findCycleStart()->val_;
         }
 };
 
 int main() {
-    
+
+    vector<int> list = {1, 2, 3, 4, 5};
+    NodeOps list1;
+    list1.createCircularList(list);
+    list1.printCircularList();
+    bool check = list1.detectCycle();
+    if (check) {
+        cout << "List is Circular\n";
+    }else {
+        cout << "List is not Circular\n";
+    }
+    cout << "MeetNode: " << list1.printMeetNode() << "\n";
+    cout << "MeetNode: " << list1.printStartNode() << "\n";
+    list1.removeCycle();
+    check = list1.detectCycle();
+    if (check) {
+        cout << "List is Circular\n";
+    }else {
+        cout << "List is not Circular\n";
+    }
+    list1.printElement();
     return 0;
 }
 
