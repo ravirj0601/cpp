3c8a49108cb70a17ddc1af5e6e92dac0b5257ad4
ravirj0601 <ravirj0601@gmail.com>
2025-09-29 22:22:54 +0530

    Today's minimal progress

---
+++ July_2025_DSA/linkedList/mergeList.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+/*
+ *
+ * class Node{};
+ *
+ * class NodeOps{
+ *  private
+ *      -   head
+ *      -   clearList(){
+ *              while
+ *                  delete each node
+ *          }
+ *  public  
+ *      -   NodeOps(){}
+ *      -   createList(){}
+ *      -   printList(){}
+ *      -   ~NodeOps(){
+ *          -   call crearList();
+ *      }
+ * };
+ *
+ * Node* mergeList(head1, head2){
+ *      Node newHead;
+ *      Node curr;
+ *      Node curr1 = head1;
+ *      Node curr2 = head2;
+ *      while (curr1 != null || curr2 != null){
+ *      -  if ( curr1 != null){
+ *          -   thiscurr = curr1
+ *          -   thiscurr = thiscurr.next
+ *          -   curr1 = curr1.next
+ *      }
+ *      -  if (curr2 != null){
+ *          -   thiscurr = curr2
+ *          -   thiscurr = thiscurr.next
+ *          -   curr2 = curr2.next
+ *      }
+ *      return newhead;
+ * }
+ */
+
+int main() {
+    
+    return 0;
+}
+
