63b76e865fc4e1ebdf1a3e4f2661aa993b7cc57c
ravirj0601 <ravirj0601@gmail.com>
2025-10-05 23:37:10 +0530

    feat: Implement StackUsingArray with dynamic array

--- July_2025_DSA/Stacks/stackWithArray.cpp
+++ July_2025_DSA/Stacks/stackWithArray.cpp
@@ -3,26 +3,72 @@
 using namespace std;
 class StackUsingArray{
     private:
-        vector<int> arr;
+        int* arr;
         int top = -1;
+        int capacity = 0;
     public:
-        StackUsingArray() = default;
-        // StackUsingArray(int size): {}
-        void push(int val){
-            
+        StackUsingArray(): arr(new int[4]){}            // default Constr
+        StackUsingArray(int size):capacity(size) {
+            arr = new int[capacity];
+        }                                               // parameterized Constr
+        void push(int val){                             // push()
+            if (top == capacity-1) {
+                cout << "Out Of Bound....!\n";
+                return;
+            } 
+            top++;
+            arr[top] = val;
+        }
+        int pop(){                      // pop()
+            if (isEmpty()) {
+                cout << "Stack is Empty...!\n";
+                return -1;
+            }
+            int temp = arr[top];
+            top--;
+            return temp;
+        }
+        int peek(){                     // Peek()
+            if (isEmpty()) {
+                return -1;
+            }
+            int temp = arr[top];
+            return temp;
+        }
+        bool isEmpty(){                 //  isEmpty
+            if (top == -1) {
+                return true;
+            }
+            return false;
+        }
+        bool isFull(){                  // isFull()
+            if (top == capacity-1) {
+                return true;
+            }
+            return false;
+        }
+        int size(){                    // size()
+            return top+1;
+        }
+        ~StackUsingArray(){             // Destructor
+            delete[] arr;
         }
 
-        int pop(){
-            return -1;
-        }
-
-        bool isEmpty(){
-            
-        }
 };
 
 int main() {
-    
+    StackUsingArray atA1(3);
+    atA1.push(4);
+    atA1.push(5);
+    atA1.push(6);
+    atA1.push(7);
+    atA1.push(9);
+    cout << atA1.pop() << "\n";
+    cout << atA1.pop() << "\n";
+    cout << atA1.isEmpty() << "\n";
+    cout << atA1.isFull() << "\n";
+    cout << atA1.peek() << "\n";
+    cout << atA1.size() << "\n";
     return 0;
 }
 
